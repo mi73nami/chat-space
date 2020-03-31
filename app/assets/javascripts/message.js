343d6698acf8b2ff85d2a27f8e7b0e0907154032
mi73nami <62558833+mi73nami@users.noreply.github.com>
2020-03-31 21:15:55 +0900

    add-automatic-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message) {
     if (message.content && message.image) {
-      var html = `<div class = "message">
+      var html = `<div class = "message" data-message-id = ${message.id}>
                     <div class = "message__user">
                       <p class = "message__user__name">${message.user_name}</p>
                       <p class = "message__user__time">${message.created_at}</p>
@@ -12,7 +12,7 @@ $(function(){
                     </div>
                   </div>`
     } else if (message.content) {
-        var html = `<div class = "message">
+        var html = `<div class = "message" data-message-id = ${message.id}>
                     <div class = "message__user">
                       <p class = "message__user__name">${message.user_name}</p>
                       <p class = "message__user__time">${message.created_at}</p>
@@ -22,7 +22,7 @@ $(function(){
                     </div>
                   </div>`
     } else if (message.image) {
-        var html = `<div class = "message">
+        var html = `<div class = "message" data-message-id = ${message.id}>
                       <div class = "message__user">
                         <p class = "message__user__name">${message.user_name}</p>
                         <p class = "message__user__time">${message.created_at}</p>
@@ -58,4 +58,37 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+  
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.message-list').append(insertHTML);
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
